89d794f3c5abb92bf49fb3733ed3c40ace1eb419
Suguhito <69892326+Suguhito65@users.noreply.github.com>
2021-03-18 19:09:00 +0900

    nodeインストール

---
+++ backend/app/Http/Controllers/TodosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    public function index() {
+        $todos = Todo::all();
+        return view('todos.index')->with('todos',$todos);
+    }
+}
